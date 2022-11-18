8915a90fbd6c8c62c27d45448255ac20449ab22d
TheMapotakes test <themapotakestest@gmail.com>
2022-11-18 12:16:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { convertToAudio, convertStreamToAudio } = require("@bbc/convert-to-audio"
 const fs = require("fs");
 const url = "https://download.ted.com/talks/KateDarling_2018S-950k.mp4";
 const audioFileOutput = "./ted-talk.wav";
+let http = require('http');
 
 convertToAudio(url, audioFileOutput)
   .then(newFile => {
@@ -19,3 +20,29 @@ convertStreamToAudio(url, writeStream)
   .catch(err => {
     console.error(err);
   });
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
+http.createServer(handleRequest).listen(8000); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
